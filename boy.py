ee76eb5d985b54a89fdbbd2bb5f4b209b62d5f0d
Uday1vADDE <vaddeudayvarma0420@gmail.com>
2025-09-08 00:51:58 -0500

    one more added

--- boy.py
+++ boy.py
@@ -2,6 +2,6 @@ gender=input("enter your gender:")
 if gender=="male":
     print("i am a good boy")
     print("may be a boy or trans too")
-    
+    print("may be unborn child too")
 else:
     print("i am a good girl")
